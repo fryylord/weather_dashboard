b83b3ce1ea9fe88132994c438d72a2b1557f00c6
fryylord <rob@mchays.com>
2022-10-20 19:53:27 -0500

    added jumbotron date/time

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,8 @@
 const apiKey = '7513e3f067ff846492d0f20633a62466'
+var timeDisplayEl = $('#dayTime');
+    
+function displayTime() {
+  var rightNow = moment().format('hh:mm:ss a [on] MMM DD, YYYY ');
+  timeDisplayEl.text(rightNow);
+  }
+  setInterval(displayTime, 1000);

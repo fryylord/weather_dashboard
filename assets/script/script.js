4b220dd38de08bc7af0c3698b03e1fe6aa4c8b57
fryylord <rob@mchays.com>
2022-10-22 09:56:01 -0500

    basic five day forcast added

--- assets/script/script.js
+++ assets/script/script.js
@@ -37,6 +37,7 @@ $().ready(function () {
           citiesList.append(currentCity);
           currentCity.on('click', function (e) {
               e.preventDefault();
+              $('#fiveDay').empty();
               callAPI($(this).attr('data-city'));
           })
       });
@@ -54,6 +55,11 @@ $().ready(function () {
           $('#current-weather').append($('<p>').text('Humidity: ' + data.main.humidity + '%').addClass('card-text'));
           $('#current-weather').append($('<p>').text('Wind Speed: ' + data.wind.speed + 'MPH').addClass('card-text'));
 
+          var lat = data.coord.lat;
+          var lon = data.coord.lon;
+          
+          displayForecast(lat,lon)
+          
           var iconURL = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
           var iconElement = $('<img>').attr('src', iconURL)
           iconElement.attr('alt', data.weather[0].description);
@@ -61,8 +67,32 @@ $().ready(function () {
       })
   }
 
+  function displayForecast(lat, lon) {
+    $.ajax({ 
+        url: 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + '&units=imperial',
+        method: "GET",
+    }).then(function (response) {
+        var arrayList = response.list;
+        for (var i = 0; i < arrayList.length; i++) {
+            if (arrayList[i].dt_txt.split(' ')[1] === '12:00:00') {
+                console.log(arrayList[i]);
+                var cityMain = $('<div>');
+                cityMain.addClass('col forecast bg-primary text-white ml-3 mb-3 rounded>');
+                var date5 = $("<h5>").text(response.list[i].dt_txt.split(" ")[0]);
+                var image = $('<img>').attr('src', 'http://openweathermap.org/img/w/' + arrayList[i].weather[0].icon + '.png');
+                var degreeMain = $('<p>').text('Temp : ' + arrayList[i].main.temp + ' °F ');               
+                var humidityMain = $('<p>').text('Humidity : ' + arrayList[i].main.humidity + '%');
+                var windMain = $('<p>').text('Wind Speed : ' + arrayList[i].wind.speed + 'MPH');                
+                cityMain.append(date5).append(image).append(degreeMain).append(humidityMain).append(windMain);
+                $('#fiveDay').append(cityMain);
+            }
+        }
+    });
+};
+
   searchButton.on('click', function (e) {
-      e.preventDefault();
+    $('#fiveDay').empty();  
+    e.preventDefault();
       var city = searchText.val();
       insertToLocal(searchText.val());
       searchText.val('');
@@ -82,3 +112,4 @@ $().ready(function () {
   })
 
   displayRecentCities();})
+  

290c516eb50862dc4b1881be6741d37c1f5d2363
fryylord <rob@mchays.com>
2022-10-21 20:09:04 -0500

    basic structure+current weather

--- assets/script/script.js
+++ assets/script/script.js
@@ -19,9 +19,6 @@ $().ready(function () {
   var searchInput = $('#search-input');
   var searchBtn = $('#search-btn');
   var citiesList = $('#cities-list');
-  var forecastContainer = $('#forecast-container');
-
-  var uvi;
 
   function insertToLocal(city) {
       if (weatherHistory.cities.includes(city) === false) {
@@ -52,15 +49,6 @@ $().ready(function () {
       return `(${month}/${day}/${year})`;
   }
 
-
-  function getDateEpoch(milliseconds) {
-      var myDate = new Date(milliseconds * 1000);
-      var day = myDate.getDate();
-      var month = myDate.getMonth() + 1;
-      var year = myDate.getFullYear();
-      return `${month}/${day}/${year}`
-  }
-
   function callAPI(city) {
       var urlQuery = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey + '&units=imperial';
       $.ajax({
@@ -80,55 +68,7 @@ $().ready(function () {
           $('#current-weather').append($('<p>').text('Temperature: ' + data.main.temp + '°').addClass('card-text'));
           $('#current-weather').append($('<p>').text('Humidity: ' + data.main.humidity + '%').addClass('card-text'));
           $('#current-weather').append($('<p>').text('Wind Speed: ' + data.wind.speed + 'MPH').addClass('card-text'));
-
-          var lat = data.coord.lat;
-          var lon = data.coord.lon;
-
-          getForecast(lat, lon)
       })
-  }
-
-  function getForecast(lat, lon) {
-      var queryURL = 'api.openweathermap.org/data/2.5/forecast/daily?lat=' + lat + '&lon=' + lon + 'appid=' + apiKey + '&units=imperial';
-
-      $.ajax({
-          url: queryURL,
-          method: 'GET'
-      }).then(function (data) {
-          forecastContainer.empty();
-          uvi = data.current.uvi;
-
-          var bgColor = '';
-          if(uvi < 3) {
-              bgColor = 'bg-success';
-          } else if (uvi < 6) {
-              bgColor = 'bg-warning';
-          } else {
-              bgColor = 'bg-danger';
-          }
-
-          for (let i = 1; i < 6; i++) {
-              let dayData =  data.daily[i];
-              var forecastCard = $('<div>');
-              forecastCard.addClass(['card', 'text-white', 'bg-primary', 'border-light', 'mb-3']);
-
-              var forecastCardBody = $('<div>').addClass('card-body');
-              forecastCard.append(forecastCardBody);
-
-              $('<h3>').addClass('card-title').text(getDateEpoch(dayData.dt)).appendTo(forecastCardBody);
-              var forecastIconURL = 'https://openweathermap.org/img/wn/' + dayData.weather[0].icon + '@2x.png';
-              $('<img>').attr('src', forecastIconURL).attr('alt', dayData.weather[0].description).css('width', '50%').appendTo(forecastCardBody);
-              $('<p>').addClass('card-text').text('Temp: ' + dayData.temp.day + '°').appendTo(forecastCardBody);
-              $('<p>').addClass('card-text').text('Humidity: ' + dayData.humidity + '%').appendTo(forecastCardBody);
-
-              forecastCard.appendTo(forecastContainer);
-          }
-      
-          var uvIndex =  $('<p>').addClass('card-text').text('UV index: ');
-          uvIndex.appendTo($('#current-weather'));
-          uvIndex.append($('<span>').text(uvi).addClass(['badge', bgColor]));
-
-      });
   }
 
 
